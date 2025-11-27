05be9224397543a883affa454cd8056697fecf4e
Divyansh Jain <divyanshjain@gkmit.co>
2025-11-27 16:41:10 +0530

    chore: add CORS config in server.js

--- src/server.js
+++ src/server.js
@@ -16,7 +16,13 @@ const PORT = process.env.PORT || 8000;
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://learnx-lms.vercel.app",
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
